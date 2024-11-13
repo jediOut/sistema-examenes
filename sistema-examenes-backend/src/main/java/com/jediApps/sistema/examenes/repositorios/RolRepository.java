87939e22879f7f96345f4b7af62c6b161bb2e319
jediOut <91569234+jediOut@users.noreply.github.com>
2024-11-13 17:22:51 -0600

    Delete sistema-examenes-backend directory

--- sistema-examenes-backend/src/main/java/com/jediApps/sistema/examenes/repositorios/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jediApps.sistema.examenes.repositorios;
-
-import com.jediApps.sistema.examenes.entidades.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, Long>{
-    
-}
